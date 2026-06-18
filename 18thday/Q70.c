1d4b8d0a37a771b162031b921bee2f1c69d8c0b5
IshanSinghal26 <ishanxsinghal@gmail.com>
2026-06-18 17:07:34 +0530

    uploaded 18th day assignment

--- 18thday/Q70.c
+++ 18thday/Q70.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int arr[100], n, i, j, temp;
+    int arr[100], n, i, j, minIndex, temp;
 
     printf("Enter the number of elements: ");
     scanf("%d", &n);
@@ -11,15 +11,19 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    // Bubble Sort
+    // Selection Sort
     for(i = 0; i < n - 1; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        minIndex = i;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 
     printf("Sorted array:\n");
--- 18thday/Q72.c
+++ 18thday/Q72.c
@@ -11,18 +11,18 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    // Bubble Sort
+    // Sort in descending order
     for(i = 0; i < n - 1; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 
-    printf("Sorted array:\n");
+    printf("Array in descending order:\n");
     for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
